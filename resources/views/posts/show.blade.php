a2c879f4226077d4e326f31e4d03f8de9421ee7e
nyanmyohtet <nyanmyohtet.bx@gmail.com>
2020-07-02 19:15:33 +0630

    Add UPDATE, DELETE post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,4 +8,11 @@
     </div>
     <hr>
     <small>Written on {{$post->created_at}}</small>
+    <div>
+        <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+    </div>
+    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+        {!!Form::hidden('_method', 'DELETE')!!}
+        {!!Form::submit('Delete', ['class' => 'btn btn-danger'])!!}
+    {!!Form::close()!!}
 @endsection
